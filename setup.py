beb1c517b8fcfdb1fa317a827ec7f1c2873adcce
Hector Robertson <hectormrobertson@gmail.com>
2024-11-16 12:12:01 +0000

    DONE! I figured this out and I cleaned up all the egg files and that. peace out

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 setup(
     name='pws256',
-    version='1.1.0',
+    version='1.1.1',
     description="pws256: A module that is used for users and passwords, and also has validation methods to check passwords with strings",
-    packages=find_packages(include=["*"], exclude=["_setup", "src", ".github", "srcimport", "usersrcimport"]),
+    packages=find_packages(include=["src"]),
     author='Hector Robertson'
 )
